db469b42ee66e7040b2b7151670671005bd0caa8
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-05-03 18:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function exactMatch(drivers, match) {
     let matches = false;
     
     for (const key in match) {
-      matches = driver[key] === match
+      matches = driver[key] === match[key];
     }
   })
 }
